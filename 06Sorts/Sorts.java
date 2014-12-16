06bfb66a00bd1f6f1b90e8c7238048d492e42049
Abhimannu Majumder <abhimannum97@gmail.com>
2014-12-16 13:20:42 -0500

    started

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,1 +1,23 @@
-public
+public class Sorts{
+
+    public static void bubble(int[] c){
+	int index = 0;
+	int length = c.length;
+	for (int i = index; i < c.length; i++){
+	    int x = c[index+1];
+	    for (int u = 
+	    if (c[index] > x) {
+		c[index+1] = c[index];
+		c[index] = x;
+	    }
+		
+	    
+	    
+	    
+    
+    }
+
+
+
+}
+
