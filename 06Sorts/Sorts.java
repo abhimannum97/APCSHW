25fc8d2049b29e006d68a260b313c0af6bdab3d2
Abhimannu Majumder <abhimannum97@gmail.com>
2014-12-18 04:20:17 -0500

    insertion sort

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -4,20 +4,20 @@ public class Sorts{
     public static void main(String[]args){
 	
 	Random rand = new Random();
-	int[] x = new int[5];
-	for (int i = 0; i < 5; i++){
+	int[] x = new int[10];
+	for (int i = 0; i < 10; i++){
 	    x[i] = rand.nextInt(40);
 	}
 	
 	System.out.print(Arrays.toString(x));
-        Sorts.bubble(x);
+        Sorts.insertion(x);
 	System.out.println(Arrays.toString(x));
 
     }
 
     public static void bubble(int[] c){
+	int hold;
 	for (int i = 0; i < c.length-1; i++){
-	    int hold = 0;
 	    for (int x = 0; x < c.length-i-1; x++){
 		if (c[x] > c[x+1]){
 		    hold = c[x];
@@ -26,6 +26,25 @@ public class Sorts{
 		}
 	    }
 	}
+    }
+
+    public static void insertion(int[] c){
+	int hold;
+	for (int i = 1; i < c.length; i++){
+	    hold = c[i];
+	    for (int x = i-1; (x >= 0) && (hold < c[x]); x--){
+		c[i] = c[x];
+		c[x] = hold;
+		hold = c[x];
+	    }
+	}
+    }
+		     
+
+	    
+	    
+}
+
     }
 
 	    
